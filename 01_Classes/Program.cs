9a42af926dcb6cb03148ebac9870ab45e87dff4d
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-11-06 13:25:04 +0300

    Add project files.

---
+++ 01_Classes/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace _01_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Sınıflar nerelerde oluşturulabilir?
+
+            // - namespace içinde
+            // - namespace dışında
+            // - ayrı bir class dosyası içinde (*)
+
+
+            #endregion
+
+            #region Personel Örneği
+            // Personel kayıt sistemi
+            // Yapı olarak sınıf oluşturuyoruz. Ve ayrı bir dosyada olacak şekilde.. "clsPersonel" sınıfımızın ismi olacak.
+            
+            // Nesne tanımlama yapıyorum
+
+            clsPersonel nur = new clsPersonel();
+            clsPersonel ahmet= new clsPersonel();
+            clsPersonel cekdar= new clsPersonel();
+            clsPersonel oyku = new clsPersonel();
+            clsPersonel tarik = new clsPersonel();
+            clsPersonel umut= new clsPersonel();
+            clsPersonel ayca= new clsPersonel();
+            clsPersonel hazal = new clsPersonel();
+            clsPersonel yusuf= new clsPersonel();
+            clsPersonel eyup= new clsPersonel();
+            clsPersonel umit = new clsPersonel();
+
+            // dummy pers bilgiler
+
+            int maasilk = 1000;
+            int maasson = 20000;
+
+            nur.setPersonelBilgi("Nur Sena", "ÖZTÜRK", new Random().Next(maasilk, maasson));
+            ahmet.setPersonelBilgi("Ahmet", "GÖKÇAY", new Random().Next(maasilk, maasson));
+            oyku.setPersonelBilgi("Nisa Öykü", "YURDAGÜL", new Random().Next(maasilk, maasson));
+            tarik.setPersonelBilgi("Tarık Ziya", "KOCABOY", new Random().Next(maasilk, maasson));
+            umut.setPersonelBilgi("Umut", "KUTLU", new Random().Next(maasilk, maasson));
+            ayca.setPersonelBilgi("Emine Ayça", "BAŞARAN", new Random().Next(maasilk, maasson));
+            hazal.setPersonelBilgi("Hazal", "ATIŞ", new Random().Next(maasilk, maasson));
+            yusuf.setPersonelBilgi("M.Yusuf", "KÜSER", new Random().Next(maasilk, maasson));
+            eyup.setPersonelBilgi("Eyüp Yasin", "İPEK", new Random().Next(maasilk, maasson));
+            umit.setPersonelBilgi("Ümit", "KARAÇİVİ", new Random().Next(maasilk, maasson));
+
+            Console.WriteLine(nur.getPersonelBilgi());
+            Console.WriteLine(ahmet.getPersonelBilgi());
+            Console.WriteLine(oyku.getPersonelBilgi());
+            Console.WriteLine(tarik.getPersonelBilgi());
+            Console.WriteLine(umut.getPersonelBilgi());
+            Console.WriteLine(ayca.getPersonelBilgi());
+            Console.WriteLine(hazal.getPersonelBilgi());
+            Console.WriteLine(yusuf.getPersonelBilgi());
+            Console.WriteLine(eyup.getPersonelBilgi());
+            Console.WriteLine(umit.getPersonelBilgi());
+
+            #endregion
+
+            #region Öğrenci-Sınıf-Not-GeçtiKaldı
+            // yani bir classım olacak
+            // öğrenci tanımlanabilecek-ad-soyad-sınıf-not1-not2
+            //(ekrandan 5 öğrenci parametrik ekran alınabilir--daha sonra class a gönderilebilir.)
+            // öğrencinin GK durumu cw yapılacak %60(2.sınav) %40(1.sınav) kuralı uygulanabilir
+
+            #endregion
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ 01_Classes/clsPersonel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Classes
+{
+    internal class clsPersonel
+    {
+        // Öncelikle sınıfımın içinde kullanacağım değişgenleri/fields tanımlamaları
+
+        // Sınıfın kimliğini hazırladık bir anlamda...
+        public int PersonelID; // dışa açık personel no
+        public string Ad; // dışa açık personelin adı
+        public string Soyad; // dışa açık personelin soyadı
+        public string Unvan;
+        public byte Yas;
+        public int Maas;
+        public int IzinGunu;
+
+        // Sınıfın kendi içsel metotları olabilir. Dışardan da erişilebilir...eğer hak verilirse.
+        
+        // bir metot yazılacak bunun da görevi tanımlaması yapılan personelin tüm bilgilerini döken bir metot
+        public string getPersonelBilgi()
+        {
+            if (Unvan != null)
+                return $"Personel Oluşturuldu...Bilgileri şunlardır..\n\n-> Personel Ad {Ad}\n-> Personel Soyad {Soyad}\n-> Personel Maaş {Maas} PNG Kina\n->Personel Ünvan {Unvan}\n-> Personel İzin Günü {IzinGunu}\n\n";
+            else
+                return "Personel Oluşturulamadı...Kontrol ediniz..";
+
+        }
+        public void setPersonelBilgi(string pAd,string pSoyad,int pMaas)
+        {
+            // Adı,soyadı,maas bilgisi verilen personelin ünvan ve izin günü ataması yapan bir metot...
+
+
+
+            // öncelikle değerlerim düzgün geliyormu...kontrolu
+            // düzgünse eğer
+            // Maas bilgisi 10000 PNG Kina dan yüksekse demekki bu kişiye izin günü ve bir ünvan atayacak bu sınıf/metot
+            // Kurallar:
+            // - Maas 10000-14000(dahil) PNG Kina bu kişi Ünvan:Kıdemli Personel-İzin günü: 20 gün izin günü tanımlansın
+            // - Maas 14001-15500(dahil) PNG Kina bu kişi Kıdemli+ Personel-İzin günü 24 gün izin günü tanımlansın
+            // - Maas >15500(dahil) PNG Kina bu kişi Uzman Personel-İzin günü 30 gün izin günü tanımlansın
+            // eğer hiçbiri uymuyorsa bildiğin Junior...tatil matil yok...
+            // eğer maas bilgisi yoksa hata mesajı versin
+
+
+            // öncelikle ad,soyad gelmişmi kontrolu yaparak işe başlayabiliriz.
+
+            if (pAd.Length >0 && pSoyad.Length >0) // ad ve soyad dolu gelmiştir.
+            {
+                // maaş bilgisini de kriterlere göre kontrolü.
+                if(pMaas >= 10000)
+                {
+                    // şimdi ilk önce benim sınıfımın alanlarının initial değerlerini belirleyeyim
+
+                    this.Ad = pAd;
+                    this.Soyad = pSoyad;
+                    this.Maas= pMaas;
+                    this.IzinGunu = 14; // default değer verdim daha sonra duruma göre değişebilecek.
+
+                    if (pMaas >=10000 && pMaas <=14000)
+                    {
+                        this.Unvan = "Kıdemli Personel";
+                        this.IzinGunu = 20;
+                    }
+                    else if (pMaas > 14001 && pMaas <= 15500)
+                    {
+                        this.Unvan = "Kıdemli+ Personel";
+                        this.IzinGunu = 24;
+                    }
+                    else if (pMaas >15501)
+                        {
+                        this.Unvan = "Uzman Personel";
+                        this.IzinGunu = 30;
+                    }
+                    else
+                    {
+                        this.Unvan = "Bildiğin Junior";
+                    }
+
+                    }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("\nPersonel Oluşturulamadı...\n\nİşletmede bir personelin en az 10000 PNG kina alması lazımdır...\n");
+                }
+                }
+            }
+        }
+
+
+
+
+}
