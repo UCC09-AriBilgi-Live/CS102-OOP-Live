5af4ea3ce14acf01f2432e0c372e16f8d59be97e
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-11-06 13:45:37 +0300

    Some changes

--- 01_Classes/clsStudent.cs
+++ 01_Classes/clsStudent.cs
@@ -8,5 +8,23 @@ namespace _01_Classes
 {
     internal class clsStudent
     {
+        // Sınıfın kimliğini hazırladık bir anlamda...
+        public string OgrAd; // dışa açık personelin adı
+        public string OgrSoyad; // dışa açık personelin soyadı
+        public string OgrSinif;
+        public byte Not1;
+        public byte Not2;
+        public byte Ortalama;
+
+        public void setInfos()
+        {
+
+        }
+
+        public int getStatus()
+        {
+            return 0;
+        }
+
     }
 }
