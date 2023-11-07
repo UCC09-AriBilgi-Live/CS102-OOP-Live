2bb1d21eee3e991643a76103d7ee34b33fc1b320
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-11-07 13:35:48 +0300

    some changes

--- 02_AccessModifiers/Program.cs
+++ 02_AccessModifiers/Program.cs
@@ -51,23 +51,93 @@
 
             #region Inline
 
-            User user = new User();
+            //User user = new User();
 
-            user.Name = "ÜK";
-            user.Location = "Estonia";
-            user.Age = 58;
+            //user.Name = "ÜK";
+            //user.Location = "Estonia";
+            //user.Age = 58;
 
-            user.getUserDetails();
-            Console.WriteLine("Lütfen bir tuşa basınız....");
+            //user.getUserDetails();
+            //Console.WriteLine("Lütfen bir tuşa basınız....");
+            //#endregion
+
+            //#region protected
+            //clsMovies movies = new clsMovies();
+
+            //Console.WriteLine(  "Movie Name : {0}",movies.movieName);
+
             #endregion
 
-            #region protected
-            clsMovies movies = new clsMovies();
+            #region Another sample
+            // Bir bookstoreumuz var.burada kitapların nosu, adı, yazarı tutulacaktır. ve ekrandan giriş alınarak sanki bir bookdb oluşturulacaktır. ve istendiği zaman kitap listesi öğrenilebilecektir.
+            // Not : 3 kitap için mesela yapılabilir
 
-            Console.WriteLine(  "Movie Name : {0}",movies.movieName);
+            // cr(console.readline)(3 satır)--> 1.kitap--> kitap1
+            // nesne yaratılacak
+            // nesneye ekrandan alınan bilgiler gömülecek
 
+            // cr(console.readline)(3 satır)--> 2.kitap--> kitap2
+            // nesne yaratılacak
+            // nesneye ekrandan alınan bilgiler gömülecek(set)
+
+            // 5 kitap bittikten sonra get ilede herbir kitabın veri içeriği cw yapılabilir...
+            int BID;
+            string BName;
+            string BAuthor;
+
+            Console.Clear();
+
+            #region Kitap1
+            // 1.kitap için...
+            Console.Write("Book ID : ");
+            BID= Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Book Name : ");
+            BName = Console.ReadLine();
+
+            Console.Write("Book Author : ");
+            BAuthor = Console.ReadLine();
+
+            clsBook book1= new clsBook(); // nesne oluşturuldu
+            book1.setBook(BID, BName, BAuthor);
             #endregion
 
+            #region Kitap2
+            // 2.kitap için...
+            Console.Write("Book ID : ");
+            BID = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Book Name : ");
+            BName = Console.ReadLine();
+
+            Console.Write("Book Author : ");
+            BAuthor = Console.ReadLine();
+
+            clsBook book2 = new clsBook(); // nesne oluşturuldu
+            book2.setBook(BID, BName, BAuthor);
+            #endregion
+
+            #region Kitap3
+            // 3.kitap için...
+            Console.Write("Book ID : ");
+            BID = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Book Name : ");
+            BName = Console.ReadLine();
+
+            Console.Write("Book Author : ");
+            BAuthor = Console.ReadLine();
+
+            clsBook book3 = new clsBook(); // nesne oluşturuldu
+            book3.setBook(BID, BName, BAuthor);
+            #endregion
+
+            // ekrana bastırma kısmı
+
+            Console.WriteLine(book1.getBook() + "\n\n");
+            Console.WriteLine(book2.getBook() + "\n\n");
+            Console.WriteLine(book3.getBook() + "\n\n");
+            #endregion
 
             Console.ReadKey();
         }
---
+++ 02_AccessModifiers/clsBook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_AccessModifiers
+{
+    internal class clsBook
+    {
+        int BookID;
+        string BookName;
+        string Author;
+
+        public void setBook(int prmBookID, string prmBookName,string prmAuthor)
+        {
+            BookID = prmBookID;
+            BookName = prmBookName;
+            Author = prmAuthor;
+
+        }
+
+        public string getBook()
+        {
+            string BookDefinition;
+
+            BookDefinition= string.Empty;
+
+            BookDefinition= "Book No : " + BookID.ToString() + " BookName : " + BookName + " Author : "+ Author;
+
+            return BookDefinition;
+        }
+
+    }
+}
