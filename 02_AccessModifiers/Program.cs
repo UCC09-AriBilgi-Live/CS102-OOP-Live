c4b6f8b993fca952a549c3c2cca1ce468d7eb1f7
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-11-07 10:50:38 +0300

    Some changes

---
+++ 02_AccessModifiers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _02_AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region public
+            // Örneğimizde bir Tutorial bilgilerinin bulunağı bir örnek. ve bunu ayrı bir sınıf dosyası içinde tutacağım.
+
+            //clsTutorial tutorial = new clsTutorial();
+
+            //tutorial.setTutorial(1, "C# Eğitimi");
+
+            //Console.WriteLine(tutorial.getTutorialName()); // set etmiş olduğum değerleri okuyacağım(get)
+
+
+            #endregion
+
+
+            #region Private
+            // Örneğimizde bir Student bilgilerinin bulunağı bir örnek. ve bunu ayrı bir sınıf dosyası içinde tutacağım.
+            clsStudent student = new clsStudent();
+
+            //Console.WriteLine("Name : " + student.Name);
+
+            student.getName();
+
+
+
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02_AccessModifiers/clsTutorial.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_AccessModifiers
+{
+    internal class clsTutorial
+    {
+        // bu sınıfta bir eğitim seti var...Bu eğitimin bir kodu var bir de tanımı var
+
+        int TutorialID;
+        string TutorialName;
+
+        // bu değerleri güncelleyecek bir metot yazmak istiyorum
+        public void setTutorial(int prmTutorialID, string prmTutorialName)
+        {
+            TutorialID = prmTutorialID;
+            TutorialName = prmTutorialName;
+        }
+
+        public string getTutorialName()
+        {
+            return TutorialName;
+        }
+
+
+    }
+}
