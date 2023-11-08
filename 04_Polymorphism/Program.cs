cb49a96bd56e7458b5bc0b0c5ec8d212bbe9f3e0
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-11-08 15:04:37 +0300

    Some changes

--- 04_Polymorphism/Program.cs
+++ 04_Polymorphism/Program.cs
@@ -105,39 +105,50 @@
             //Cat cat1 = new Cat();
             //Console.WriteLine(cat1.MakeNoise());
 
-            Staff staff = new Staff();
-            staff.Name = "Biff";
-            staff.Lastname = "Tannen";
-            staff.DailyWage = 10;
-            staff.WorkDays = 20;
+            //Staff staff = new Staff();
+            //staff.Name = "Biff";
+            //staff.Lastname = "Tannen";
+            //staff.DailyWage = 10;
+            //staff.WorkDays = 20;
 
-            Worker worker = new Worker();
-            worker.Name = "Marty";
-            worker.Lastname = "McFly";
-            worker.DailyWage = 10;
-            worker.WorkDays = 20;
+            //Worker worker = new Worker();
+            //worker.Name = "Marty";
+            //worker.Lastname = "McFly";
+            //worker.DailyWage = 10;
+            //worker.WorkDays = 20;
 
-            Manager manager = new Manager();
-            manager.Name = "Emmett";
-            manager.Lastname = "Brown";
-            manager.DailyWage = 10;
-            manager.WorkDays = 20;
+            //Manager manager = new Manager();
+            //manager.Name = "Emmett";
+            //manager.Lastname = "Brown";
+            //manager.DailyWage = 10;
+            //manager.WorkDays = 20;
 
-            Intern intern = new Intern();
-            intern.Name = "Lorraine";
-            intern.Lastname = "Baines";
-            intern.DailyWage = 10;
-            intern.WorkDays = 20;
+            //Intern intern = new Intern();
+            //intern.Name = "Lorraine";
+            //intern.Lastname = "Baines";
+            //intern.DailyWage = 10;
+            //intern.WorkDays = 20;
 
-            Console.WriteLine("Staff: " + staff.Name + " " + staff.Lastname + " Salary: " + staff.Salary);
-            Console.WriteLine("Staff: " + worker.Name + " " + worker.Lastname + " Salary: " + worker.Salary);
-            Console.WriteLine("Staff: " + manager.Name + " " + manager.Lastname + " Salary: " + manager.Salary);
-            Console.WriteLine("Staff: " + intern.Name + " " + intern.Lastname + " Salary " + intern.Salary); 
+            //Console.WriteLine("Staff: " + staff.Name + " " + staff.Lastname + " Salary: " + staff.Salary);
+            //Console.WriteLine("Staff: " + worker.Name + " " + worker.Lastname + " Salary: " + worker.Salary);
+            //Console.WriteLine("Staff: " + manager.Name + " " + manager.Lastname + " Salary: " + manager.Salary);
+            //Console.WriteLine("Staff: " + intern.Name + " " + intern.Lastname + " Salary " + intern.Salary); 
 
-// Daily Workout 01
-// Çember,Kare,Dikdörtgen için bir alan hesaplama programı yazılacaktır
-// Herbir nesne için hesaplamayı yapacak gerekli  bilgiler atandıktan sonra hesaplama işlemleri ayrı ayrı yapılıp ekrana basılacaktır...
+    // Nightly Workout 01
 
+    // Çember,Kare,Dikdörtgen için bir alan hesaplama programı yazılacaktır
+    // Herbir nesne için hesaplamayı yapacak gerekli  bilgiler atandıktan sonra hesaplama işlemleri ayrı ayrı yapılıp ekrana basılacaktır...
+
+            // öncelikle bir taban(base) class yaratarak işe başlanabilir (Hesaplama isminde)
+
+            // işte bu base classdan Cember,Kare,Dikdortgen sub-classları inherit edin(kalıtın..ki ayrı ayrı aynı tanımlanmış metodu kullanabilin.)
+
+            // Bazı kabuller yapın.Yani initial değerler verin. Yani constructor yaratın.
+
+            // alan hesapları yaparken birden fazla nesne olduğu için metodlarınızda override yaparak kullanın
+
+
+            // Daily Workout 02
 
 
                     Console.ReadKey();
---
+++ 05_Constructors/Program.cs
@@ -0,0 +1,163 @@
+﻿namespace _05_Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programci prg1 = new Programci();
+            //Programci prg2 = new Programci(23, "Ziya");
+            //Programci prg3 = new Programci(27, "Kamuran", "Kamiloğlu");
+            //Programci prg4 = new Programci(30, "Hayrettin", "Kütükçü", "C#");
+
+
+            //prg1.showOzellikler();
+            //prg2.showOzellikler();
+            //prg3.showOzellikler();
+            //prg4.showOzellikler();
+
+            urun urun = new urun();
+            urun.Ad = "Televizyon";
+            urun.AlisFiyat = 500;
+            urun.Miktar = 5;
+            urun.SatisFiyat = urun.AlisFiyat * 2;
+            urun.Kar = (urun.SatisFiyat - urun.AlisFiyat) * urun.Miktar;
+
+            Console.WriteLine($"Ürün Adı : {urun.Ad}");
+            Console.WriteLine($"Ürün Alış Fiyatı : {urun.AlisFiyat} TL.");
+            Console.WriteLine($"Ürün Satış Fiyatı : {urun.SatisFiyat} TL.");
+            Console.WriteLine($"Miktar : {urun.Miktar} adet");
+            Console.WriteLine($"Karınız : {urun.Kar} TL.dir.Allah bereket versin...");
+
+            Console.ReadKey();
+        }
+    }
+
+    #region Sample 1
+    //class Programci
+    //{
+    //    int yasi;
+    //    string adi;
+    //    string soyadi;
+    //    string kullandigiDil;
+
+    //    // Hic parametre almayan bir yapılandırıcı..(constructor)
+    //    public Programci()
+    //    {
+    //        this.adi = null;
+    //        this.yasi = 0;
+    //        this.soyadi = null;
+    //        this.kullandigiDil = null;
+    //    }
+
+    //    // İsmini ve yasini alan bir yapılandırıcı..
+    //    public Programci(int yasi, string adi)
+    //    {
+    //        this.adi = adi;
+    //        this.yasi = yasi;
+
+    //        this.soyadi = null;
+    //        this.kullandigiDil = null;
+    //    }
+
+    //    // İsmini, soyismini ve yasini alan bir yapılandırıcı..
+    //    public Programci(int yasi, string adi, string soyadi)
+    //    {
+    //        this.adi = adi;
+    //        this.yasi = yasi;
+    //        this.soyadi = soyadi;
+    //        this.kullandigiDil = null;
+    //    }
+
+    //    // İsmini, soyismini kullandığı dili ve yasini alan bir yapılandırıcı..
+    //    public Programci(int yasi, string adi, string soyadi, string kullandigiDil)
+    //    {
+    //        this.adi = adi;
+    //        this.yasi = yasi;
+    //        this.soyadi = soyadi;
+    //        this.kullandigiDil = kullandigiDil;
+    //    }
+
+    //    public void showOzellikler()
+    //    {
+    //        if (this.yasi != 0)
+    //            Console.WriteLine("Yasi : " + this.yasi);
+    //        else Console.WriteLine("Yasi bilgisi elimizde yok şu anda...");
+
+    //        if (this.adi != null)
+    //            Console.WriteLine("Adı : " + this.adi);
+    //        else Console.WriteLine("Adi bilgisi elimizde yok şu anda...");
+
+    //        if (this.soyadi != null)
+    //            Console.WriteLine("Soyadı : " + this.soyadi);
+    //        else Console.WriteLine("Soyadı bilgisi elimizde yok şu anda...");
+
+    //        if (this.kullandigiDil != null)
+    //            Console.WriteLine("Kullandığı Programlama dili : " + this.kullandigiDil);
+    //        else Console.WriteLine("Hangi Dili kullanığını bilmiyoruz be... :-(\n");
+    //    }
+    //}
+
+    #endregion
+
+    #region Sample 2
+
+    class urun
+    {
+        string ad;
+        public urun()  //Kurucu metot
+        {
+            ad = "";
+            grupNo = 0;
+            miktar = 0;
+            alisFiyat = 0;
+            kar = 0;
+        }
+
+        public string Ad
+        {
+            get { return ad; }
+            set { ad = value; }
+        }
+        int grupNo;
+
+        public int GrupNo
+        {
+            get { return grupNo; }
+            set { grupNo = value; }
+        }
+        
+        int miktar;
+        public int Miktar
+        {
+            get { return miktar; }
+            set { miktar = value; }
+        }
+
+        int alisFiyat;
+        public int AlisFiyat
+        {
+            get { return alisFiyat; }
+            set { alisFiyat = value; }
+        }
+
+        int satisFiyat;
+        public int SatisFiyat
+        {
+            get { return satisFiyat; }
+            set { satisFiyat = value; }
+        }
+
+        int kar;
+        public int Kar
+        {
+            get { return kar; }
+            set { kar = value; }
+        }
+
+
+
+    }
+
+    #endregion
+}
+
