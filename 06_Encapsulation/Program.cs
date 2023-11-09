d14fa18bd3ee71a812a2824ed06220559c146897
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-11-09 12:34:12 +0300

    Some changes

--- 06_Encapsulation/Program.cs
+++ 06_Encapsulation/Program.cs
@@ -23,14 +23,102 @@
 
             #region get/set olayı
 
-            clsBank bankgs = new clsBank();
+            //clsBank bankgs = new clsBank();
 
-            bankgs.setBalanceCaps(500); // set ediyorum.
+            //bankgs.setBalanceCaps(500); // set ediyorum.
 
-            Console.WriteLine($"Hesabınızdaki miktar : {bankgs.getBalanceCaps()} EURO dur..."); // get ediyorum.
+            //Console.WriteLine($"Hesabınızdaki miktar : {bankgs.getBalanceCaps()} EURO dur..."); // get ediyorum.
 
 
             #endregion
+
+            #region Eğer sınıfımızı tasarlarken Kapsülleme(encapsulation) prensibine uymazsak ne olur?
+            // açıklamalar gelecek
+
+            //clsBank bank2= new clsBank();
+
+            //bank2.Balance = 1000;
+
+            //Console.WriteLine($"Bankadaki miktarınız {bank2.Balance} PNG Kina'dır...\n\n"  );
+
+            //bank2.Balance = -5000;
+
+            //Console.WriteLine($"Bankadaki miktarınız {bank2.Balance} PNG Kina'dır...\n\n");
+
+            // Yukardaki kısım pek istenen bir durum değil.çünküm miktara negatif bir değer girebliyorum...Hata...
+
+            //try
+            //{
+            //    int deger;
+
+            //    clsBank bank3 = new clsBank();
+
+            //    Console.WriteLine("Lütfen miktarı giriniz : ");
+            //    deger=Convert.ToInt32(Console.ReadLine());
+
+            //    bank3.SetAmount(deger);
+
+            //    Console.WriteLine($"\n\n Düzgün durum\n\nBankanızdaki miktar : {bank3.GetAmount()} PNG Kinadır\n\n");
+
+            //    Console.WriteLine("Lütfen yeni miktarı giriniz : ");
+            //    deger = Convert.ToInt32(Console.ReadLine());
+
+
+            //    bank3.SetAmount(deger); // eğer negatif bir sayı girersem set edemeycek geriye hata gelecek ve dolayısıyla catch e düşecek
+
+            //    Console.WriteLine($"\n\nBankanızdaki miktar : {bank3.GetAmount()} PNG Kinadır\n\n");
+
+            //}
+            //catch (Exception hata)
+            //{
+
+            //    Console.WriteLine(hata.Message);
+            //}
+
+
+            #endregion
+
+            #region get/set olayı değişgenler/property lere de uygulunabilir ...işte bu durumda get/set keywordlerini kullabilirim
+
+            //try
+            //{
+            //    clsBank bank4 = new clsBank();
+
+            //    bank4.Amount1 = 1000; // hemen geri planda otomatikman set metodunu çalıştırır
+
+            //    Console.WriteLine("Miktar : {0}\n\n",bank4.Amount1  ); // get metodu devreye girecek
+
+            //    bank4.Amount1 = -5000;
+            //    Console.WriteLine("Miktar : {0}\n\n", bank4.Amount1); // get metodu devreye girecek
+
+
+            //}
+            //catch (Exception hata)
+            //{
+            //    Console.WriteLine(  hata.Message);
+
+            //}
+
+            try
+            {
+                clsBank bank5 = new clsBank();
+
+                bank5._Amount2 = 1000;
+
+                Console.WriteLine($"Miktar : {bank5._Amount2}\n\n"); // direkt olarak değişgenik kendisine ulasabiliyorum.
+
+                bank5._Amount2 = -1000;
+
+                Console.WriteLine($"Miktar : {bank5._Amount2}\n\n"); // direkt olarak değişgenik kendisine ulasabiliyorum.
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Lütfen pozitif sayı girin dedik lo...");
+            }
+
+
+            #endregion
+
         }
     }
 }
--- 06_Encapsulation/clsBank.cs
+++ 06_Encapsulation/clsBank.cs
@@ -19,9 +19,18 @@ namespace _06_Encapsulation
             Console.WriteLine("Hesaptaki miktar : {0} EURO dur",this.Balance);
         }
 
+        public int getAmount() // Miktarı öğrenmek
+        {
+            return this.Balance;
+        }
         public void setBalance(int balance)
         {
             this.Balance = this.Balance + balance;
+        }
+
+        public void setAmount(int balance)
+        {
+            this.Balance =  balance;
         }
 
 
@@ -54,11 +63,68 @@ namespace _06_Encapsulation
 
         }
 
+        #endregion
 
+        #region Kurallara uymazsak
+
+        public int Amount;
+
+        public int GetAmount()
+        {
+            return Amount;
+        }
+
+        public void SetAmount(int Amount) // kendi set metodum.
+        {
+            if (Amount > 0)
+            {
+                this.Amount = Amount;
+            }
+            else
+            {
+                throw new Exception("Lütfen pozitif bir sayı girmelisiniz....");
+            }
+
+
+        }
+
+
+        #endregion
+
+        #region get/set olayı değişgenler/property lere de uygulunabilir ...işte bu durumda get/set keywordlerini kullabilirim
+
+        private int _Amount1;
+
+        // get/set keyword kullanımının eski yöntemi
+
+        public int Amount1
+        {
+            get // içindeki değeri almak istersem _Amount1 in 
+            {
+                return _Amount1;
+            }
+            
+            set  // içini doldurmak için de set
+            { 
+                if (value < 0)
+                {
+                    throw new Exception("Lütfen girdiğiniz miktara dikkat ediniz....Pozitif giriniz...");
+                }
+                else
+                {
+                    _Amount1 = value; // value olarak ne idüğü belirsiz o an
+                }
+
+            }
+        }
+
+        public int _Amount2 { get;  set; } // daha kısa bir yöntem
 
 
 
         #endregion
+
+
 
 
     }
