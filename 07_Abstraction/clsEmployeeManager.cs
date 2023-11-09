97eb07e11877115da4921e9ab390e869ecf0264d
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-11-09 14:49:31 +0300

    Some changes

---
+++ 07_Abstraction/clsEmployeeManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Abstraction
+{
+    // işletme içersinde "Beden İşçisi" statusundeki kişiler için  böyle bir sınıf oluşturuyorum ve Base class dan kendime kalıtıyorum..ve bu sınıf için gereken kodları yazıyor.
+    internal class clsEmployeeManager : clsEmployeeBaseManager
+    {
+        // Constructor ...
+        public clsEmployeeManager() 
+        {
+            // bu sınıf "clsEmployeeManager" ilk yaratıldığı anda default bazı tanımlamalar yapabilmek için...
+            setTitle("Beden İşçisi");
+        
+        }
+        public override void doRaise() // seyyanen zam
+        {
+            double salary= getSalary(); // maaşını bilmem lazım ki zam yapabileyim.
+
+            double raise = salary + 500; // burası zamlı maaş olacak
+
+            setSalary(raise);
+
+        }
+
+        public override void doRaise(double prmRaiseRate) // zam + prim
+        {
+            double salary = getSalary(); // zam öncesi maaş
+
+            double raise = salary + (salary * prmRaiseRate/100);
+
+            setSalary(raise); //  yeni maas set edilecek...
+
+        }
+    }
+}
---
+++ 07_Abstraction/clsEngineerManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Abstraction
+{
+    internal class clsEngineerManager : clsEmployeeBaseManager
+    {
+
+        public clsEngineerManager() 
+        {
+            setTitle("Müyendis");
+        }
+        public override void doRaise()
+        {
+            double salary = getSalary(); // maaşını bilmem lazım ki zam yapabileyim.
+
+            double raise = salary + 2500; // burası zamlı maaş olacak
+
+            setSalary(raise);
+        }
+
+        public override void doRaise(double prmRaiseRate)
+        {
+            double salary = getSalary(); // zam öncesi maaş
+
+            double raise = salary + (salary * prmRaiseRate / 100) + (salary * 2);
+
+            setSalary(raise); //  yeni maas set edilecek...
+        }
+    }
+}
