5c65447e3ad195e03e0d6bf473d0077957e1217d
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-11-09 10:41:52 +0300

    some changes

---
+++ 06_Encapsulation/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _06_Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Normal Durum
+
+            clsBank bank = new clsBank();
+
+            bank.AccountNumber = 1234567890123;
+            bank.Name = "Santander Bank";
+            bank.Balance = 500; // elden yatırdım
+
+            bank.getBalance();
+
+            bank.setBalance(1000); // bir yerlerden geldi
+
+            bank.getBalance();
+
+            #endregion
+
+
+            #region get/set olayı
+
+
+
+            #endregion
+        }
+    }
+}
---
+++ 06_Encapsulation/clsBank.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Encapsulation
+{
+    internal class clsBank
+    {
+        #region Normal durum
+
+        public long AccountNumber; // hesap no
+        public string Name;
+        public int Balance; // Miktar
+
+        public void getBalance() // Miktarı öğrenmek
+        {
+            Console.WriteLine("Hesaptaki miktar : {0} EURO dur",this.Balance);
+        }
+
+        public void setBalance(int balance)
+        {
+            this.Balance = this.Balance + balance;
+        }
+             
+        
+
+
+
+        #endregion
+
+
+    }
+}
